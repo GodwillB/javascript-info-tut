774b1e507747eb933a395db322a6f07c7193eae8
GodwillB <iamgodwillb@gmail.com>
2020-08-23 00:31:51 +0300

    copy from one variable to another

--- index.js
+++ index.js
@@ -11,3 +11,34 @@ alert (name);
 let jina = 'Godwill', miaka = 26, anapotoka = 'Kakamega';
 
 alert (residence);
+
+
+
+// We can also change it as many times as we want:,  the old data is removed from the variable
+
+
+
+let message;
+
+message = 'Doho';
+
+message = 'Lonely'
+
+alert(message);
+
+
+// We can also declare two variables and copy data from one into the other.
+
+let jiina = 'Godwill2';
+
+let namename;
+
+
+// copy
+
+namename = jiina;
+
+alert(jiina);
+alert(namename);
+
+
