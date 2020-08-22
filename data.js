61c5a304723d4caab640101aaed895e83e467f3a
GodwillB <iamgodwillb@gmail.com>
2020-08-23 01:21:07 +0300

    data typess

--- data.js
+++ data.js
@@ -1,22 +1,64 @@
-// Number
-// =================================
+// Data Types
 
-let n = 123;
-n = 12.345;
-
-// The number type represents both integer and floating point numbers.
-
-// There are many operations for numbers, e.g. multiplication *, division /, addition +, subtraction -, and so on.
-
-// Besides regular numbers, there 
+// There are 7 data types
 
 
-// Booleans
 
-let b = false;
 
-if (b) {
-	console.log('Booleans are ass');
+// Boolean. true and false
+
+var data = true;
+
+if (data) {
+  console.log("Booleans rule!")
 } else {
-	console.log('Booleans niiiiice')
+  console.log("Booleans are lame.")
 }
+
+
+
+// null. is an assignment value that means “no value”
+
+var n = null;
+console.log(n * 32);
+
+
+
+// undefined. means a variable has not been declared, or has been declared but has not yet been assigned a value
+
+var a;
+console.log(a + 2);
+
+
+
+// Number. 42 or 3.14159.
+
+var num = 3.6;
+var ber = 6.4;
+console.log(num + ber);
+
+
+
+// String. "Hey!"
+
+var name = "Godwill";
+console.log("Hi! My name is " + name);
+
+
+
+// Symbol (new in ECMAScript 2015). A data type whose instances are unique and immutable.
+
+var sym1 = Symbol("foo");
+var sym2 = Symbol("foo");
+console.log(sym1 === sym2)
+console.log(String(sym1) === String(sym2))
+
+
+
+// Object - An object is a collection of properties, and a property is an association between a name (or key) and a value.
+
+var myCar = new Object();
+myCar.make = "Ford";
+myCar.model = "Mustang";
+
+console.log(myCar.make);
