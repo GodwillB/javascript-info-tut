0cba02e0e83f9c89245658567eda14a82e353c57
GodwillB <iamgodwillb@gmail.com>
2020-08-23 00:25:03 +0300

    multiple variables in this multiline style

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ let age = 26;
 let residence = 'Kakamega';
 
 alert (name);
+
+// multiple variables in this multiline style:
+
+let jina = 'Godwill', miaka = 26, anapotoka = 'Kakamega';
+
+alert (residence);
