c43392bd33e4287cf82a8a37587064293d5cba61
Godwill Barasa <33608094+GodwillB@users.noreply.github.com>
2021-04-09 10:59:58 +0300

    Create codeStructure.js

---
+++ codeStructure.js
@@ -0,0 +1,20 @@
+// We can have as many statements in our code as we want. Statements can be separated with a semicolon.
+
+// here we split “Hello World” into two alerts:
+
+alert('Hello'); alert('World');
+
+
+// statements are written on separate lines to make the code more readable:
+alert('Hello');
+alert('World');
+
+// In most cases, a newline implies a semicolon. But “in most cases” does not mean “always”!
+
+// There are cases when a newline does not mean a semicolon. For example:
+
+alert(3 +
+1
++ 2);
+
+// Nested comments are not supported!
