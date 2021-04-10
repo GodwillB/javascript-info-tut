24f68bb8e91bd986ce8352a1c5dd332bdace7c32
Godwill Barasa <33608094+GodwillB@users.noreply.github.com>
2021-04-10 20:46:35 +0300

    Update basicOperatorsMath.js

--- basicOperatorsMath.js
+++ basicOperatorsMath.js
@@ -1,8 +1,64 @@
-n operand – is what operators are applied to. For instance, in the multiplication of 5 * 2 there are two operands: the left operand is 5 and the right operand is 2. Sometimes, people call these “arguments” instead of “operands”.
+// n operand – is what operators are applied to. For instance, in the multiplication of 5 * 2 there are two operands: the left operand is 5 and the right operand is 2. Sometimes, people call these “arguments” instead of “operands”.
 
-An operator is unary if it has a single operand. For example, the unary negation - reverses the sign of a number:
+// An operator is unary if it has a single operand. For example, the unary negation - reverses the sign of a number:
 
 let x = 1;
 
 x = -x;
 alert( x ); // -1, unary negation was applied
+
+// An operator is binary if it has two operands. The same minus exists in binary form as well:
+
+let x = 1, y = 3;
+alert( y - x ); // 2, binary minus subtracts values
+
+// The remainder operator %, despite its appearance, is not related to percents.
+
+// The result of a % b is the remainder of the integer division of a by b.
+
+// For instance:
+
+alert( 5 % 2 ); // 1, a remainder of 5 divided by 2
+alert( 8 % 3 ); // 2, a remainder of 8 divided by 3
+
+
+// The exponentiation operator a ** b raises a to the power of b.
+
+// In school maths, we write that as ab.
+
+// For instance:
+
+alert( 2 ** 2 ); // 2² = 4
+alert( 2 ** 3 ); // 2³ = 8
+alert( 2 ** 4 ); // 2⁴ = 16
+
+// Just like in maths, the exponentiation operator is defined for non-integer numbers as well.
+
+// For example, a square root is an exponentiation by ½:
+
+alert( 4 ** (1/2) ); // 2 (power of 1/2 is the same as a square root)
+alert( 8 ** (1/3) ); // 2 (power of 1/3 is the same as a cubic root)
+
+Usually, the plus operator + sums numbers.
+
+But, if the binary + is applied to strings, it merges (concatenates) them:
+
+let s = "my" + "string";
+alert(s); // mystring
+
+// The plus + exists in two forms: the binary form that we used above and the unary form.
+
+// The unary plus or, in other words, the plus operator + applied to a single value, doesn’t do anything to numbers. But if the operand is not a number, the unary plus converts it into a number.
+
+// For example:
+
+// No effect on numbers
+let x = 1;
+alert( +x ); // 1
+
+let y = -2;
+alert( +y ); // -2
+
+// Converts non-numbers
+alert( +true ); // 1
+alert( +"" );   // 0
