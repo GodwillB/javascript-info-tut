3f024be0653e96e27e6c4b2fb08d066897c61716
Godwill Barasa <33608094+GodwillB@users.noreply.github.com>
2021-04-10 20:42:37 +0300

    Create basicOperatorsMath.js

---
+++ basicOperatorsMath.js
@@ -0,0 +1,8 @@
+n operand – is what operators are applied to. For instance, in the multiplication of 5 * 2 there are two operands: the left operand is 5 and the right operand is 2. Sometimes, people call these “arguments” instead of “operands”.
+
+An operator is unary if it has a single operand. For example, the unary negation - reverses the sign of a number:
+
+let x = 1;
+
+x = -x;
+alert( x ); // -1, unary negation was applied
