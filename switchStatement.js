15038952740bcad0eafa773614a06a5180110ddb
Godwill Barasa <33608094+GodwillB@users.noreply.github.com>
2021-04-13 09:09:25 +0300

    Create switchStatement.js

---
+++ switchStatement.js
@@ -0,0 +1,35 @@
+// he switch has one or more case blocks and an optional default.
+
+// It looks like this:
+
+switch(x) {
+  case 'value1':  // if (x === 'value1')
+    ...
+    [break]
+
+  case 'value2':  // if (x === 'value2')
+    ...
+    [break]
+
+  default:
+    ...
+    [break]
+}
+
+// An example of switch (the executed code is highlighted):
+
+// let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    alert( 'Too small' );
+    break;
+  case 4:
+    alert( 'Exactly!' );
+    break;
+  case 5:
+    alert( 'Too big' );
+    break;
+  default:
+    alert( "I don't know such values" );
+}
