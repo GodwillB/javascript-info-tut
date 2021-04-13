9d68ac4223609aa68273dd6b633784cf634978ae
Godwill Barasa <33608094+GodwillB@users.noreply.github.com>
2021-04-13 09:25:36 +0300

    Update switchStatement.js

--- switchStatement.js
+++ switchStatement.js
@@ -33,3 +33,20 @@ switch (a) {
   default:
     alert( "I don't know such values" );
 }
+
+// If there is no break then the execution continues with the next case without any checks.
+
+// An example without break:
+
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    alert( 'Too small' );
+  case 4:
+    alert( 'Exactly!' );
+  case 5:
+    alert( 'Too big' );
+  default:
+    alert( "I don't know such values" );
+}
