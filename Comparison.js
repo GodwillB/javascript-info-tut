667640c81a31f2fd1b319f909f0e10f1be792b45
Godwill Barasa <33608094+GodwillB@users.noreply.github.com>
2021-06-12 13:27:20 +0300

    Update Comparison.js

--- Comparison.js
+++ Comparison.js
@@ -31,3 +31,33 @@ alert( '01' == 1 ); // true, string '01' becomes a number 1
 
 alert( true == 1 ); // true
 alert( false == 0 ); // true
+
+// When comparing values of different types, JavaScript converts the values to numbers.
+
+// For example:
+
+alert( '2' > 1 ); // true, string '2' becomes a number 2
+alert( '01' == 1 ); // true, string '01' becomes a number 1
+
+// A funny consequence
+// It is possible that at the same time:
+
+// Two values are equal.
+// One of them is true as a boolean and the other one is false as a boolean.
+// For example:
+
+let a = 0;
+alert( Boolean(a) ); // false
+
+let b = "0";
+alert( Boolean(b) ); // true
+
+alert(a == b); // true!
+
+// Strict equality
+// A regular equality check == has a problem. It cannot differentiate 0 from false:
+
+alert( 0 == false ); // true
+// The same thing happens with an empty string:
+
+alert( '' == false ); // true
